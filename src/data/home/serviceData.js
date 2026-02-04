c687e5464f45b3ecf846c8ca220bee435cefea79
ToBiyo <fabio.web.arru@gmail.com>
2026-02-04 11:09:20 +0100

    feat: extract services link data into data layer

---
+++ src/data/home/serviceData.js
@@ -0,0 +1,29 @@
+const servizi = [
+  {
+    title: "PMU",
+    linkUrl: "/servizi/pmu",
+    bg: "bg-[#F5E8D0]",
+  },
+  {
+    title: "TRATTAMENTI VISO",
+    linkUrl: "/servizi/trattamenti-viso",
+    bg: "bg-[#E4BFA1]",
+  },
+  {
+    title: "TRATTAMENTI CORPO",
+    linkUrl: "/servizi/trattamenti-corpo",
+    bg: "bg-[#EAC1B5]",
+  },
+  {
+    title: "LAMINAZIONE",
+    linkUrl: "/servizi/laminazione",
+    bg: "bg-[#C4E9D7]",
+  },
+  {
+    title: "MACCHINARI",
+    linkUrl: "/servizi/macchinari",
+    bg: "bg-[#A4F4CF]",
+  },
+];
+
+export default servizi;
