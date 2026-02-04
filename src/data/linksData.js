34ad4ffff2a6ef517d4c3d7904bd1f4c8977ccd8
ToBiyo <fabio.web.arru@gmail.com>
2026-02-04 12:09:24 +0100

    feat: extract links data into data layer

---
+++ src/data/linksData.js
@@ -0,0 +1,36 @@
+const links = [
+  {
+    path: "/",
+    name: "Home",
+  },
+  {
+    path: "/chi-sono",
+    name: "Chi Sono",
+  },
+  {
+    path: "/servizi/pmu",
+    name: "Pmu",
+  },
+  {
+    path: "/servizi/trattamenti-viso",
+    name: "Trattamenti Viso",
+  },
+  {
+    path: "/servizi/trattamenti-corpo",
+    name: "Trattamenti Corpo",
+  },
+  {
+    path: "/servizi/laminazione",
+    name: "Laminazione",
+  },
+  {
+    path: "/servizi/macchinari",
+    name: "Macchinari",
+  },
+  {
+    path: "/contatti",
+    name: "Contatti",
+  },
+];
+
+export default links;
