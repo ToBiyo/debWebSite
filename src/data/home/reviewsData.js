49f6551bec4a455842af08768ad905d0c1965808
ToBiyo <fabio.web.arru@gmail.com>
2026-02-04 11:16:51 +0100

    feat: extract reviews data into data layer

---
+++ src/data/home/reviewsData.js
@@ -0,0 +1,28 @@
+const reviews = [
+  {
+    review:
+      "Ragazza seria e molto professionale. I trattamenti richiesti sono stati eseguiti in modo preciso e meticoloso, il risultato è stato ECCELLENTE! Il tutto accompagnato da gentilezza e solarità.",
+    user: "Francesca L.",
+    rating: 5,
+  },
+  {
+    review:
+      "Esperienza super positiva. Il team è professionale e molto simpatico, mi ha consigliato il trattamento più adatto alle mie esigenze. Un ambiente accogliente dove ci si sente subito a proprio agio.",
+    user: "Giorgia M.",
+    rating: 5,
+  },
+  {
+    review:
+      "Centro estetico di altissima qualità. Ho apprezzato la disponibilità e la puntualità, ma soprattutto la meticolosità con cui viene eseguito ogni servizio. Risultati visibili dalla prima seduta. Super consigliato!",
+    user: "Marta G.",
+    rating: 5,
+  },
+  {
+    review:
+      "Sono stata in questo centro mentre ero di passaggio ma se dovesse ricapitare non esiterei a riprenotare. La ragazza che mi ha assistita è brava, professionale e di una gentilezza squisita, come tutto il personale.",
+    user: "Claudia D.",
+    rating: 5,
+  },
+];
+
+export default reviews;
